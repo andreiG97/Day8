29e5c4a3cd936ee14d3fbae371fc16198f02473a
Andrei G <andrei.grideanu@gmail.com>
2021-10-04 12:44:45 +0300

    Improved version Bug fix for high number of shifts Now can use every character available including numbers Option to keep running

--- CaesarCypher.py
+++ CaesarCypher.py
@@ -1,25 +1,21 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-
 
 def encrypt(text_to_encode, nr_shifts):
     encoded = ''
     text_array = list(text_to_encode)
     for i in range(0, text_array.__len__()):
-        current_index = alphabet.index(text_array[i])
-        if current_index + nr_shifts < alphabet.__len__():
-            text_array[i] = alphabet[current_index + nr_shifts]
+        if text_array[i] in alphabet:
+            current_index = alphabet.index(text_array[i])
+            if current_index + nr_shifts < alphabet.__len__():
+                text_array[i] = alphabet[current_index + nr_shifts]
+            else:
+                shifter = current_index + nr_shifts - alphabet.__len__()
+                text_array[i] = alphabet[shifter % 26]
         else:
-            shifter = current_index + nr_shifts - alphabet.__len__()
-            text_array[i] = alphabet[shifter]
+            pass
     encoded = "".join(text_array)
     return encoded
-
-
-
 
 
 def decrypt(text_to_decode, nr_unshifts):
@@ -34,12 +30,25 @@ def decrypt(text_to_decode, nr_unshifts):
 
 
 def main(direct):
+
     if direct == 'encrypt':
         return encrypt(text, shift)
     return decrypt(text, shift)
 
 
-print(main(direction))
+should_run = True
+while should_run:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    print(main(direction))
+    run = input('Yes or no')
+
+    if run.lower() == 'yes':
+        should_run = True
+    else:
+        should_run = False
 
 
 
